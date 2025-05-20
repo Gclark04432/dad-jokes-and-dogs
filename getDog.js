d685497a549e01d75e1d6990ff5ed136b2aae519
Gary Clark <garyclark@Garys-MacBook-Pro-2.local>
2025-05-19 20:06:52 -0700

    Initial commit

---
+++ getDog.js
@@ -0,0 +1,19 @@
+async function getDog() {
+const url = "https://dog.ceo/api/breed/labrador/images/random";  
+
+try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    document.getElementById('dog-pic')['src'] = json.message
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getDog()      
+        
